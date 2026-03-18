01a4efba26963eaa7173d4a01f6588e6503ff3e7
AlexYoGit <JokubeitAlex@gmail.com>
2026-03-18 01:20:46 +0100

    add preparation for auth stuff

---
+++ kawasys.webappsample.Shared/CircuitSecurityState.cs
@@ -0,0 +1,11 @@
+﻿namespace kawasys.webappsample.Shared
+{
+    public class CircuitSecurityState
+    {
+        //preparation for propagating auth state, since WebApps run each connection in its own circuit, i dont need to reauth everything everytime i can just use the unique circuit id of the current session
+        // since all remotes are in the shell, they can get the Auth Web Apps Circuit ID locally and then cross reference in the backend for its auth state
+        public bool IsAuthorized { get; private set; }
+
+        public void Authorize() => IsAuthorized = true;
+    }
+}
